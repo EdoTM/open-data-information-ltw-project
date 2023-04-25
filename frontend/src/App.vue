424340bb4369b5dca06c4b814b2a39eeb18e55b7
edotm <edokyactus@gmail.com>
2023-04-25 19:14:19 +0200

    feat: add margin to app body

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,5 +7,7 @@ const userName = "";
 
 <template>
     <Navbar :is-logged="isLogged" :user-name="userName"/>
-    <router-view/>
+    <div class="m-5">
+        <router-view/>
+    </div>
 </template>
