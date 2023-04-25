e6c956d285f8b7a9d1701b63250d1b1dee7020ed
edotm <edokyactus@gmail.com>
2023-04-26 00:00:42 +0200

    build: add bootstrap icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap-icons/font/bootstrap-icons.css"
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "./Home.vue";
 import Signup from "./Signup.vue";
