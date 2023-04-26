e142dc9a61b69e90ffd2a2bf922f54d555ee57f2
edotm <edokyactus@gmail.com>
2023-04-26 16:12:06 +0200

    feat: add username and email taken validation

--- frontend/src/Signup.vue
+++ frontend/src/Signup.vue
@@ -1,7 +1,7 @@
 <script setup>
 import axiosInstance from "./axiosInstance";
 import md5 from "crypto-js/md5";
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
 function generatePasswordMD5(plainPassword) {
   return md5(plainPassword).toString();
@@ -24,6 +24,9 @@ function handleResponse(response) {
   console.log("Login successful");
 }
 
+const usernameTaken = ref(false);
+const emailTaken = ref(false);
+
 function sendSignUpRequest() {
   const plainPassword = document.getElementById("signupInputPassword").value;
 
@@ -42,15 +45,33 @@ function sendSignUpRequest() {
     .catch((error) => {
       console.log(error);
       if (error.response.status === 409) {
-        // if ("Username" in error.response.data.error) 
+        usernameTaken.value = false;
         if (error.response.data.error.includes("Username")) {
           signupError.value = "Username already taken";
+          document
+            .getElementById("signupInputUsername")
+            .setCustomValidity("Username already taken");
+          usernameTaken.value = true;
         } else if (error.response.data.error.includes("Email")) {
           signupError.value = "Email already taken";
+            document
+                .getElementById("signupInputEmail")
+                .setCustomValidity("Email already registered");
+            emailTaken.value = true;
         }
       }
       console.log("LoginError: ", signupError);
     });
+}
+
+function resetUsernameValidity() {
+  document.getElementById("signupInputUsername").setCustomValidity("");
+  usernameTaken.value = false;
+}
+
+function resetEmailValidity() {
+    document.getElementById("signupInputEmail").setCustomValidity("");
+    emailTaken.value = false;
 }
 
 function checkSignUpFormValidity() {
@@ -62,10 +83,9 @@ function checkSignUpFormValidity() {
         event.preventDefault();
         event.stopPropagation();
       } else {
-          sendSignUpRequest();
+        sendSignUpRequest();
       }
       form.classList.add("was-validated");
-
     },
     false
   );
@@ -80,21 +100,25 @@ onMounted(() => {
   <div class="mx-auto mb-4" style="max-width: 300px">
     <h1>Sign up</h1>
     <form novalidate id="form-coso" @submit.prevent="() => {}">
-      <div class="form-floating mb-3">
+      <div :class="['form-floating mb-3' + (emailTaken ? ' is-invalid' : '')]">
         <input
           type="email"
           class="form-control"
           id="signupInputEmail"
           required
-          placeholder="s"
+          placeholder="Email"
           pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[a-z]+$"
+          @input="resetEmailValidity"
         />
-        <div class="invalid-feedback">Insert a valid email address.</div>
+          <div v-if="emailTaken" class="invalid-feedback">
+              Email is already registered. Maybe you want to log in?
+          </div>
+          <div v-else class="invalid-feedback">Email is not valid.</div>
         <label for="signupInputEmail">Email address</label>
       </div>
       <div class="input-group mb-3 has-validation">
         <span class="input-group-text">@</span>
-        <div class="form-floating">
+        <div :class="['form-floating' + (usernameTaken ? ' is-invalid' : '')]">
           <input
             type="text"
             class="form-control"
@@ -103,10 +127,14 @@ onMounted(() => {
             required
             pattern="[A-Za-z0-9_-]{1,15}$"
             maxlength="15"
+            @input="resetUsernameValidity"
           />
           <label for="signupInputUsername">Username</label>
         </div>
-        <div class="invalid-feedback">Username is not valid.</div>
+        <div v-if="usernameTaken" class="invalid-feedback">
+          Username already taken.
+        </div>
+        <div v-else class="invalid-feedback">Username is not valid.</div>
       </div>
       <div class="form-floating mb-3">
         <input
@@ -156,12 +184,7 @@ onMounted(() => {
         </label>
         <div class="invalid-feedback">You must agree before submitting.</div>
       </div>
-      <button
-        type="submit"
-        class="btn btn-success mt-2"
-      >
-        Sign up
-      </button>
+      <button type="submit" class="btn btn-success mt-2">Sign up</button>
     </form>
   </div>
 </template>
