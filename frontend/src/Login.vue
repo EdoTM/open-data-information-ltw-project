9ed656a61f8b4cfae6c46ac37b2ac4c1d502a516
edotm <edokyactus@gmail.com>
2023-04-26 11:01:03 +0200

    style: reformat code

--- frontend/src/Login.vue
+++ frontend/src/Login.vue
@@ -1,74 +1,98 @@
 <script setup>
 import axiosInstance from "./axiosInstance";
 import md5 from "crypto-js/md5";
-import {ref} from "vue";
+import { ref } from "vue";
 
 function generatePasswordMD5(plainPassword) {
-    return md5(plainPassword).toString()
+  return md5(plainPassword).toString();
 }
 
 const loginError = ref("");
 
-const emits = defineEmits(["logged-in", "update:user-name", "update:user-email"])
+const emits = defineEmits([
+  "logged-in",
+  "update:user-name",
+  "update:user-email",
+]);
 
 function handleResponse(response) {
-    const json = response.data;
-    emits("logged-in", true);
-    emits("update:user-name", json.username);
-    emits("update:user-email", json.email);
-    console.log("Login successful");
+  const json = response.data;
+  emits("logged-in", true);
+  emits("update:user-name", json.username);
+  emits("update:user-email", json.email);
+  console.log("Login successful");
 }
 
 function sendLoginRequest() {
-    const plainPassword = document.getElementById("inputPassword").value;
+  const plainPassword = document.getElementById("inputPassword").value;
 
-    const login = {
-        email: document.getElementById("inputEmail").value,
-        password: generatePasswordMD5(plainPassword)
-    }
+  const login = {
+    email: document.getElementById("inputEmail").value,
+    password: generatePasswordMD5(plainPassword),
+  };
 
-    console.log(login)
+  console.log(login);
 
-    axiosInstance.post("/login", login).then(handleResponse).catch(error => {
-        console.log(error)
-        if (error.response.status === 401) {
-            loginError.value = "INVALID_CREDENTIALS";
-        } else if (error.response.status === 404) {
-            loginError.value = "USER_NOT_FOUND";
-        }
-        console.log("LoginError: ", loginError);
+  axiosInstance
+    .post("/login", login)
+    .then(handleResponse)
+    .catch((error) => {
+      console.log(error);
+      if (error.response.status === 401) {
+        loginError.value = "INVALID_CREDENTIALS";
+      } else if (error.response.status === 404) {
+        loginError.value = "USER_NOT_FOUND";
+      }
+      console.log("LoginError: ", loginError);
     });
 }
 </script>
 
 <template>
-    <div class="mx-auto mb-4" style="max-width: 300px">
-        <form @submit.prevent="sendLoginRequest">
-            <div class="mb-3">
-                <label for="inputEmail" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="inputEmail" required>
-            </div>
-            <div class="mb-3">
-                <label for="inputPassword" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword" minlength="8" required>
-            </div>
-            <div v-if="loginError === 'INVALID_CREDENTIALS'" class="alert alert-danger" role="alert">
-                Invalid password.
-            </div>
-            <div v-else-if="loginError === 'USER_NOT_FOUND'" class="alert alert-danger" role="alert">
-                User not found.
-            </div>
-            <div v-else-if="loginError === 'UNKNOWN_ERROR'" class="alert alert-danger" role="alert">
-                An error occurred.
-            </div>
-            <button type="submit" class="btn btn-primary mt-2">Login</button>
-        </form>
-    </div>
-
+  <div class="mx-auto mb-4" style="max-width: 300px">
+    <form @submit.prevent="sendLoginRequest">
+      <div class="mb-3">
+        <label for="inputEmail" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="inputEmail" required />
+      </div>
+      <div class="mb-3">
+        <label for="inputPassword" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword"
+          minlength="8"
+          required
+        />
+      </div>
+      <div
+        v-if="loginError === 'INVALID_CREDENTIALS'"
+        class="alert alert-danger"
+        role="alert"
+      >
+        Invalid password.
+      </div>
+      <div
+        v-else-if="loginError === 'USER_NOT_FOUND'"
+        class="alert alert-danger"
+        role="alert"
+      >
+        User not found.
+      </div>
+      <div
+        v-else-if="loginError === 'UNKNOWN_ERROR'"
+        class="alert alert-danger"
+        role="alert"
+      >
+        An error occurred.
+      </div>
+      <button type="submit" class="btn btn-primary mt-2">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
-}
+  name: "Login",
+};
 </script>
--- frontend/src/Navbar.vue
+++ frontend/src/Navbar.vue
@@ -1,94 +1,112 @@
 <script setup>
+import { ref } from "vue";
 
-import {ref} from "vue";
-
-const html = document.querySelector('html');
-const themeInitial = html.getAttribute('data-bs-theme');
+const html = document.querySelector("html");
+const themeInitial = html.getAttribute("data-bs-theme");
 const theme = ref(themeInitial);
 
 const emits = defineEmits(["log-out"]);
 
-
 function toggleTheme() {
-    if (theme.value === 'dark') {
-        theme.value = 'light';
-    } else {
-        theme.value = 'dark';
-    }
-        html.setAttribute('data-bs-theme', theme.value);
+  theme.value = theme.value === "dark" ? "light" : "dark";
+  html.setAttribute("data-bs-theme", theme.value);
 }
-
 
 function deleteCookie(name) {
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 }
+
 function handleLogout() {
-    deleteCookie("sessionID");
-    emits("log-out");
-    console.log("Logout successful");
+  deleteCookie("sessionID");
+  emits("log-out");
+  console.log("Logout successful");
 }
-
-
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-sm bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/">Plot</router-link>
-                    </li>
-                </ul>
-                <span class="nav-item me-2">
-                    <button class="btn btn-outline-secondary" @click="toggleTheme()" href="#">
-                        <i v-if="theme === 'dark'" class="bi bi-sun-fill" style="font-size: 1em"></i>
-                        <i v-else class="bi bi-moon-stars-fill"></i>
-                    </button>
-                </span>
-                <div v-if="isLogged === false" class="navbar-nav">
-                    <a href="#" class="nav-link me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                        Log in
-                    </a>
-                    <router-link class="btn btn-success" aria-current="page" to="/signup">Sign up</router-link>
-                </div>
-                <ul v-else class="navbar-nav">
-                    <li class="navbar-text me-3">
-                        Logged in as {{ userName }}
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-outline-secondary" aria-current="page" @click="handleLogout('sessionID');" href="#">Log out</button>
-                    </li>
-                </ul>
-            </div>
+  <nav class="navbar navbar-expand-sm bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/"
+              >Plot
+            </router-link>
+          </li>
+        </ul>
+        <span class="nav-item me-2">
+          <button
+            class="btn btn-outline-secondary"
+            @click="toggleTheme()"
+            href="#"
+          >
+            <i
+              v-if="theme === 'dark'"
+              class="bi bi-sun-fill"
+              style="font-size: 1em"
+            ></i>
+            <i v-else class="bi bi-moon-stars-fill"></i>
+          </button>
+        </span>
+        <div v-if="isLogged === false" class="navbar-nav">
+          <a
+            href="#"
+            class="nav-link me-2"
+            data-bs-toggle="modal"
+            data-bs-target="#loginModal"
+          >
+            Log in
+          </a>
+          <router-link class="btn btn-success" aria-current="page" to="/signup"
+            >Sign up
+          </router-link>
         </div>
-    </nav>
+        <ul v-else class="navbar-nav">
+          <li class="navbar-text me-3">Logged in as {{ userName }}</li>
+          <li class="nav-item">
+            <button
+              class="btn btn-outline-secondary"
+              aria-current="page"
+              @click="handleLogout()"
+              href="#"
+            >
+              Log out
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 import Login from "./Login.vue";
 
 export default {
-    name: "Navbar",
-    components: {Login},
-    props: {
-        isLogged: {
-            type: Boolean,
-            required: true
-        },
-        userName: {
-            type: String,
-        }
-    }
-}
+  name: "Navbar",
+  components: { Login },
+  props: {
+    isLogged: {
+      type: Boolean,
+      required: true,
+    },
+    userName: {
+      type: String,
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
