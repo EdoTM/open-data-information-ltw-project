878398f58c8f76fc387ebd7e6157618d33ef2391
edotm <edokyactus@gmail.com>
2023-04-27 17:59:57 +0200

    feat: add routes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,25 +4,30 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./components/Home.vue";
-import Signup from "./components/Signup.vue";
-import Report from "./components/Report.vue";
+import HomePage from "./pages/HomePage.vue";
+import SignupPage from "./pages/SignupPage.vue";
+import ReportPage from "./pages/ReportPage.vue";
+import PlotPage from "./pages/PlotPage.vue";
 
 let router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
-      component: Home,
+      component: HomePage,
     },
     {
       path: "/signup",
-      component: Signup,
+      component: SignupPage,
     },
     {
       path: "/report",
-      component: Report
-    }
+      component: ReportPage,
+    },
+    {
+      path: "/plot",
+      component: PlotPage,
+    },
   ],
 });
 
