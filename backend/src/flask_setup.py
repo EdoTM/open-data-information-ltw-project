ba451cffff9e76f535f0e7546b5d5b462f2d6a65
Frank <f.bianchi202@gmail.com>
2023-04-27 14:39:29 +0200

    add: rsa signature for cookies

--- backend/src/flask_setup.py
+++ backend/src/flask_setup.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+from Crypto.PublicKey import RSA
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.urandom(32)
+
+rsa_priv_key = RSA.generate(2048)
+app.config["RSA_PRIV_KEY"] = rsa_priv_key
+
+
 CORS(app, supports_credentials=True, origins=["http://localhost:*"])
--- backend/src/utils/utils.py
+++ backend/src/utils/utils.py
@@ -1,22 +1,46 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.number import bytes_to_long, long_to_bytes 
+from hashlib import sha256
 from flask_setup import app
 from flask import make_response
 from database import Database
 from utils.errors import *
+import os
 
 
 def generate_user_session_id(input_email: str):
-    cipher = AES.new(app.config["SECRET_KEY"], AES.MODE_ECB)
-    session_id = cipher.encrypt(pad(input_email.encode(), AES.block_size)).hex()
-    return session_id
+    IV = os.urandom(16)
+    cipher = AES.new(app.config["SECRET_KEY"], IV=IV, mode=AES.MODE_CBC)
+    rsa_priv_key = app.config["RSA_PRIV_KEY"]
+
+    session_id_plaintext = input_email
+    session_id_ciphertext = IV.hex() + cipher.encrypt(pad(session_id_plaintext.encode(), AES.block_size)).hex()
+    
+    hashed_msg = bytes_to_long(sha256(session_id_plaintext.encode()).digest())
+    
+    signature = pow(hashed_msg, rsa_priv_key.d, rsa_priv_key.n)
+
+    return  session_id_ciphertext + '-' + long_to_bytes(signature).hex()
 
 
 def get_user_from_session_id(session_id: str):
     db = Database.get_instance()
-    cipher = AES.new(app.config["SECRET_KEY"], AES.MODE_ECB)
     try:
-        email = unpad(cipher.decrypt(bytes.fromhex(session_id)), AES.block_size).decode()
+        IV = bytes.fromhex(session_id[:32])
+        cipher = AES.new(app.config["SECRET_KEY"], IV=IV, mode=AES.MODE_CBC)
+
+        session_id_ciphertext = session_id[32:].split('-')[0]
+        signature = session_id[32:].split('-')[1]
+
+        session_id_plaintext = unpad(cipher.decrypt(bytes.fromhex(session_id_ciphertext)), AES.block_size).decode()
+
+        hashed_msg = bytes_to_long(sha256(session_id_plaintext.encode()).digest())
+        rsa_pub_key = app.config["RSA_PRIV_KEY"].publickey()
+        if pow(bytes_to_long(bytes.fromhex(signature)), rsa_pub_key.e, rsa_pub_key.n) != hashed_msg:
+            raise UserNotFoundError()
+
+
     except ValueError as e:
         if "Padding is incorrect" in str(e):
             raise UserNotFoundError()
@@ -24,6 +48,8 @@ def get_user_from_session_id(session_id: str):
             raise UserNotFoundError()
         print(e)
         raise ValueError()
+    
+    email = session_id_plaintext
     user = db.get_user_by_email(email)
     return user
 
