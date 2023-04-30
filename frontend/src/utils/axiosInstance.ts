ece8edf203e9276c33e075ef0a735118ba94060f
edotm <edokyactus@gmail.com>
2023-04-30 16:46:49 +0200

    feat: use origin instead of localhost

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: window.location.origin.replace("5173", "5000"),
   withCredentials: true,
 });
 
