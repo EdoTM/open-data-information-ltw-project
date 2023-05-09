5e42850a63d79594fe8a38697f60fe0e71d86d51
edotm <edokyactus@gmail.com>
2023-05-09 10:57:04 +0200

    Revert "feat: strange background"
    This reverts commit 1780ee3717a06d1e2f5d5a96c1fa38d87ae05df2.

--- frontend/src/pages/plots/PlotMeetingsPage.vue
+++ frontend/src/pages/plots/PlotMeetingsPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import PlotElement from "../../components/PlotElement.vue";
 import plusIcon from "../../assets/plus-element.svg";
-import { computed, provide, ref, watch } from "vue";
+import { computed, provide, ref } from "vue";
 import VChart, { THEME_KEY } from "vue-echarts";
 import { SVGRenderer } from "echarts/renderers";
 import { use } from "echarts/core";
@@ -16,7 +16,6 @@ import {
 import { ECharts, EChartsOption } from "echarts";
 import CreatePostForm from "../../components/CreatePostForm.vue";
 import axiosInstance from "../../utils/axiosInstance";
-import { executeTransition } from "@vueuse/core";
 
 type PlotDataType = {
   xAxisValues: string[];
@@ -147,97 +146,45 @@ function publish() {
 }
 
 const createPostFormRef = ref<any>();
-
-const color = ref<string>();
-const color1 = [12, 130, 57];
-const color2 = [255, 255, 0];
-
-const sliderValue = ref(0);
-
-watch(sliderValue, changeSliderColor);
-
-function changeSliderColor() {
-  const value = sliderValue.value / 100;
-  const r = Math.round(color1[0] * value + color2[0] * (1 - value));
-  const g = Math.round(color1[1] * value + color2[1] * (1 - value));
-  const b = Math.round(color1[2] * value + color2[2] * (1 - value));
-  color.value = `rgb(${r}, ${g}, ${b})`;
-}
-
-function susus(event) {
-  sliderValue.value = Number.parseInt(event.target.value);
-}
-
-function handleSliderChange() {
-  const initialValue = sliderValue.value;
-  const endValue = initialValue > 50 ? 100 : 0;
-  executeTransition<number>(sliderValue, initialValue, endValue, {
-    duration: 400,
-    transition: [0.6, 0, 0.41, 0.98],
-  });
-}
-
-const ciaone = computed(() => {
-  console.log(sliderValue.value);
-  return `radial-gradient(
-    circle at ${sliderValue.value}% 1000%,
-    ${color.value} -10%,
-    transparent 100%
-  )`;
-});
 </script>
 
 <template>
-  <h1 :style="{ color }">Meetings</h1>
-  <div>
-    <div class="mx-auto d-flex" style="width: 300px">
-      <label class="form-label" for="customRange1">Nations</label>
-      <input
-        id="customRange1"
-        :value="sliderValue"
-        class="form-range mx-3"
-        type="range"
-        @change="handleSliderChange"
-        @input="susus"
+  <h1>Meetings</h1>
+  <div class="d-flex flex-wrap justify-content-center">
+    <PlotElement
+      v-for="(element, i) in elements"
+      :key="i"
+      :categories="categories"
+      :current-category="element.currentCategory"
+      :element-color="element.color"
+      :element-name="element.name"
+      class="m-2"
+      @delete="deleteElement(i)"
+      @change-category="(c) => (element.currentCategory = c)"
+    />
+    <button
+      class="btn btn-success mx-3 add-element-button my-auto"
+      type="button"
+      @click="addElement"
+    >
+      <img
+        :src="plusIcon"
+        alt="Add"
+        style="width: 1.7rem; filter: invert(100%)"
       />
-      <label class="form-label" for="customRange1">Companies</label>
-    </div>
-    <div class="d-flex flex-wrap justify-content-center">
-      <PlotElement
-        v-for="(element, i) in elements"
-        :key="i"
-        :categories="categories"
-        :current-category="element.currentCategory"
-        :element-color="element.color"
-        :element-name="element.name"
-        class="m-2"
-        @delete="deleteElement(i)"
-        @change-category="(c) => (element.currentCategory = c)"
-      />
-      <button
-        class="btn btn-success mx-3 add-element-button my-auto"
-        type="button"
-        @click="addElement"
+    </button>
+  </div>
+  <div class="my-3" style="text-align: right">
+    <button
+      class="btn btn-primary mx-3 my-auto align-items-center"
+      type="button"
+      @click="requestPlot"
+    >
+      <span class="me-2 my-auto" style="font-weight: bold; font-size: 1.3rem"
+        >Plot</span
       >
-        <img
-          :src="plusIcon"
-          alt="Add"
-          style="width: 1.7rem; filter: invert(100%)"
-        />
-      </button>
-    </div>
-    <div class="my-3" style="text-align: right">
-      <button
-        class="btn btn-primary mx-3 my-auto align-items-center"
-        type="button"
-        @click="requestPlot"
-      >
-        <span class="me-2 my-auto" style="font-weight: bold; font-size: 1.3rem"
-          >Plot</span
-        >
-        <i class="bi bi-graph-up-arrow" style="font-size: 1.5rem"></i>
-      </button>
-    </div>
+      <i class="bi bi-graph-up-arrow" style="font-size: 1.5rem"></i>
+    </button>
   </div>
   <div
     v-if="plotData"
@@ -305,31 +252,4 @@ const ciaone = computed(() => {
       </div>
     </div>
   </div>
-  <div class="bg-element" />
 </template>
-
-<style scoped>
-input[type="range"]::-webkit-slider-runnable-track {
-  height: 25px;
-  padding-top: 4px;
-}
-
-input[type="range"]::-webkit-slider-thumb {
-  aspect-ratio: 1;
-  width: 25px;
-  height: auto;
-  /*border-radius: 50%;*/
-  background: v-bind(color);
-  transition: 0ms;
-}
-
-.bg-element {
-  background: v-bind(ciaone);
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-}
-</style>
