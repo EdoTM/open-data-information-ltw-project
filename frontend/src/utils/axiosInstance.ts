11d896d1359c6ec75a68f69fd9d1caf6f190037c
edotm <edokyactus@gmail.com>
2023-05-17 18:03:07 +0200

    feat: ROBA

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BACKEND_URL: string =
   import.meta.env.VITE_BACKEND_URL ||
+  (import.meta.env.VITE_BACKEND_SAME_PORT && window.location.origin) ||
   window.location.origin.replace("5173", "5000");
 
 const axiosInstance = axios.create({
