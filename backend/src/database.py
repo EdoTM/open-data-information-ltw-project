dd8217503133513cfe3f4274c4074def1085b773
Frank <f.bianchi202@gmail.com>
2023-05-23 16:08:03 +0200

    feat: /api/getPosts returns only not hidden posts

--- backend/src/database.py
+++ backend/src/database.py
@@ -71,6 +71,7 @@ class Database:
             left join votes v2 on p.id = v2.post and v2.email = ?
             left join favorites f on p.id = f.post and f.email = ?
             left join hidden h on p.id = h.post and h.email = ?
+            where h.hidden = 0
             group by p.id
             order by p.timestamp desc
         """
